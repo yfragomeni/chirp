75fa13e6b42ce7e223e801948ada95a0b9dc12bd
yfragomeni <yfragomeni@gmail.com>
2023-05-02 20:48:23 -0300

    add Auth

---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export default withClerkMiddleware((req: NextRequest) => {
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ["/((?!static|.*\\..*|_next|favicon.ico).*)", "/"],
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,14 @@ import { type AppType } from "next/app";
 import { api } from "npm/utils/api";
 
 import "npm/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
